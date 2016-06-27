e98a45fc35735d6e861770152e42b30e49118c83
Jalal Hoseini <jalal7h@gmail.com>
2016-06-27 09:58:24 +0430

    fix dimention on litesponsor n2

--- components/litesponsor-1/litesponsor_vw.php
+++ components/litesponsor-1/litesponsor_vw.php
@@ -6,7 +6,11 @@ function litesponsor_vw(){
 	if(! $rs = dbq(" SELECT * FROM `litesponsor` ORDER BY rand() LIMIT 5 ") ){
 		e( __FUNCTION__ , __LINE__ );
 	} else while( $rw = dbf($rs) ){
-		$c.= '<a href="'.$rw['link'].'" target="_blank"><img height="40px" src="'._URL.'/'.$rw['image'].'" /></a>';
+		$sz = getimagesize($rw['image']);
+		$width = $sz[0];
+		$height = $sz[1];
+		$width = round( ($width * 40) / $height );
+		$c.= '<a href="'.$rw['link'].'" target="_blank"><img width="'.$width.'px" height="40px" src="'._URL.'/'.$rw['image'].'" /></a>';
 	}
 	$c.= "</div>\n";
 
